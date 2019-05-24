3e1abcbf15433987aab413e6ea0d789d238724d6
Patrick Kim <pkim050@gmail.com>
2019-05-24 01:36:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (number) {
+  let result = number - 42;
+  if (result < 0) {
+    result *= -1;
+  }
+  return result;
+}
+
+function distanceFromHqInFeet (number) {
+  let result = distanceFromHqInBlocks(number);
+  result *= 264;
+  return result;
+}
+
+function distanceTravelledInFeet (number1, number2) {
+  let result = number1 - number2;
+  if (result < 0) {
+    result *= -1;
+  }
+  result *= 264;
+  return result;
+}
+
+function calculatesFarePrice (number1, number2) {
+  let result = distanceTravelledInFeet(number1, number2);
+  if (result <= 400) {
+    return 0;
+  }
+  else if (result > 400 && result < 2000) {
+    result -= 400;
+    result *= 0.02;
+    return result;
+  }
+  else if (result > 2500) {
+    return "cannot travel that far";
+  }
+  else if (result > 2000 && result < 2500) {
+    return 25;
+  }
+}
