0a8e8aaa21b0fb05979b4719f1436ece4bc6d2b5
Patrick Kim <pkim050@gmail.com>
2019-05-24 01:35:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ function distanceFromHqInFeet (number) {
   result *= 264;
   return result;
 }
+
+function distanceTravelledInFeet (number1, number2) {
+  let result = number1 - number2;
+  if (result < 0) {
+    result *= -1;
+  }
+  result *= 264;
+  return result;
+}
+
+function calculatesFarePrice (number1, number2) {
+  let result = distanceTravelledInFeet(number1, number2);
+  if (result <= 400) {
+    return 0;
+  }
+  else if (result > 400 && result < 2000) {
+    result -= 400;
+    result *= 0.02;
+    return result;
+  }
+  else if (result > 2500) {
+    return "cannot travel that far";
+  }
+  else if (result > 2000 && result < 2500) {
+    return 25;
+  }
+}
