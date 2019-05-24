85027519ed2dbd606510c8993abc7f799854eab8
Patrick Kim <pkim050@gmail.com>
2019-05-24 01:24:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (number) {
+  let result = number - 42;
+  if (result < 0) {
+    result *= -1;
+  }
+  return result;
+}
+
+function distanceFromHqInFeet (number) {
+  let result = distanceFromHqInBlocks(number);
+  result *= 264;
+  return result;
+}
